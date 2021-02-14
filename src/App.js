9eb9c74f167089b9cb6090a6f7fa77babdefe923
Pedro Santos <pedrosino@gmail.com>
2021-02-14 19:39:57 -0300

    Fix App.js?

--- src/App.js
+++ src/App.js
@@ -18,22 +18,7 @@ function App() {
   const [usuario, setUsuario] = useState(localStorage.getItem('userid') || 0);
 
   return (
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
+    
 
     <UserContext.Provider value={[usuario, setUsuario]}>
       <BrowserRouter>
